7a94f95ae016ede08e7171abcde3d95d8d0e3ff7
Chris McCormick <chris@mccormick.cx>
2025-07-08 21:23:09 +0800

    Forked dashboard script to nkv.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-Download the `dashboard` command line utility:
+Command line utility to manage a Nostr-sync'ed key-value store.
+
+Download the `nkv` command line utility:
 
 ```
-wget https://github.com/chr15m/dashboard/releases/latest/download/dashboard
-chmod 755 dashboard
-./dashboard
+wget https://github.com/chr15m/nkv/releases/latest/download/nkv
+chmod 755 nkv
+./nkv
 ```
--- nkv.cljs
+++ nkv.cljs
@@ -1,5 +1,5 @@
 #!/usr/bin/env -S npx nbb
-(ns dashboard
+(ns nkv
   {:clj-kondo/config '{:lint-as {promesa.core/let clojure.core/let}}}
   (:require
     [clojure.string :as str]
@@ -12,8 +12,8 @@
 
 (useWebSocketImplementation ws)
 
-(def app-name "cx.mccormick.dashboard")
-(def nsec-file-path ".dashboard-nsec")
+(def app-name "cx.mccormick.nkv")
+(def nsec-file-path ".nkv-nsec")
 (def nostr-kind 30078)
 (def default-relays ["wss://relay.damus.io" "wss://relay.nostr.band"])
 
@@ -32,7 +32,7 @@
       nil)))
 
 (defn create-finalized-event [sk-bytes clj-content d-identifier]
-  (js/console.log "Creating event for d-identifier:" d-identifier "with content:" (pr-str clj-content))
+  (js/console.error "Creating event for d-identifier:" d-identifier "with content:" (pr-str clj-content))
   (let [encrypted-content (encrypt-content sk-bytes clj-content)
         event-template
         (clj->js
@@ -41,19 +41,19 @@
            :tags [["d" (str app-name ":" d-identifier)]
                   ["n" app-name]]
            :content encrypted-content})]
-    (js/console.log "event" event-template)
+    (js/console.error "event" event-template)
     (nostr/finalizeEvent event-template sk-bytes)))
 
 (defn publish-event [event relays]
-  (js/console.log "Publishing event to relays:" (clj->js relays))
+  (js/console.error "Publishing event to relays:" (clj->js relays))
   (p/catch
     (p/let [pool (SimplePool.)
             published (js/Promise.allSettled (.publish pool (clj->js relays) event))
             success? (seq (filter #(= (aget % "status") "fulfilled")
                                   published))]
-      (js/console.log "published" published)
+      (js/console.error "published" published)
       (if success?
-        (js/console.log "Event published to at least one relay.")
+        (js/console.error "Event published to at least one relay.")
         (do
           (js/console.error "No relay accepted the event.")
           (js/process.exit 1))))
@@ -67,7 +67,7 @@
             decoded (nostr/nip19.decode (str/trim nsec-str))]
         (if (= (aget decoded "type") "nsec")
           (do
-            (js/console.log "Loaded nsec from" nsec-file-path)
+            (js/console.error "Loaded nsec from" nsec-file-path)
             (aget decoded "data"))
           (throw (js/Error. (str "Invalid nsec format in " nsec-file-path)))))
       (catch :default e
@@ -76,18 +76,18 @@
     (let [sk-bytes (nostr/generateSecretKey)
           nsec-str (nostr/nip19.nsecEncode sk-bytes)]
       (fs/writeFileSync nsec-file-path nsec-str "utf-8")
-      (js/console.log "Generated new nsec and saved to" nsec-file-path)
+      (js/console.error "Generated new nsec and saved to" nsec-file-path)
       sk-bytes)))
 
 (defn write-value [sk-bytes key-arg value-arg relays]
-  (js/console.log (str "Attempting to write key: " key-arg ", value: " value-arg))
+  (js/console.error (str "Attempting to write key: " key-arg ", value: " value-arg))
   (let [content {:value value-arg}
         event (create-finalized-event sk-bytes content key-arg)]
     (publish-event event relays)))
 
 (defn read-value [sk-bytes pk-hex key-arg relays]
-  (js/console.log (str "Attempting to read key: " key-arg))
-  (js/console.log "pk-key" pk-hex)
+  (js/console.error (str "Attempting to read key: " key-arg))
+  (js/console.error "pk-key" pk-hex)
   (p/catch
     (p/let [pool (SimplePool.)
             query-filter (clj->js {:authors [pk-hex]
@@ -99,7 +99,7 @@
         (let [sorted-events (sort-by #(aget % "created_at") > events)
               latest-event (first sorted-events)
               decrypted (decrypt-content sk-bytes (aget latest-event "content"))]
-          (js/console.log "Latest event content decrypted:" (clj->js decrypted))
+          (js/console.error "Latest event content decrypted:" (clj->js decrypted))
           (when decrypted (:value decrypted)))))
     (fn [err] (js/console.error err))))
 
@@ -108,48 +108,50 @@
 
 (defn print-usage [summary]
   (println "Usage:")
-  (println "  dashboard <key>          Read a value for the given key.")
-  (println "  dashboard <key> <value>  Write a value for the given key.")
+  (println "  nkv <key>          Read a value for the given key.")
+  (println "  nkv <key> <value>  Write a value for the given key.")
   (println)
   (println "Options:")
   (println summary))
 
 (defn main [& args]
-  (p/let [_ (js/console.log "Dashboard script started.")] ; Initial log
-    (let [sk-bytes (load-or-generate-nsec) ; This is synchronous
-          pk-hex (pubkey sk-bytes)
-          {:keys [options arguments errors summary]} (cli/parse-opts args cli-options)
-          arg-count (count arguments)]
-      (js/console.log "Arguments parsed:" (pr-str arguments))
-      (cond
-        errors
-        (do (doseq [error errors] (println error))
-            (print-usage summary)
-            (js/process.exit 1))
+  (let [sk-bytes (load-or-generate-nsec) ; This is synchronous
+        pk-hex (pubkey sk-bytes)
+        {:keys [options arguments errors summary]} (cli/parse-opts args cli-options)
+        arg-count (count arguments)]
+    #_ (js/console.error "Arguments parsed:" (pr-str arguments))
+    (cond
+      errors
+      (do (doseq [error errors] (js/console.error error))
+          (print-usage summary)
+          (js/process.exit 1))
 
-        (:help options)
-        (do (print-usage summary)
+      (:help options)
+      (do (print-usage summary)
+          (js/process.exit 0))
+
+      (not (or (= 1 arg-count) (= 2 arg-count)))
+      (do (js/console.error "Error: Invalid number of arguments. Must be 1 or 2.")
+          (print-usage summary)
+          (js/process.exit 1))
+
+      :else
+      (let [[key-arg value-arg] arguments]
+        (if (= 2 arg-count)
+          ; Write operation
+          (p/do!
+            (write-value sk-bytes key-arg value-arg default-relays)
+            (js/console.error (str "Value set for key '" key-arg "'."))
             (js/process.exit 0))
-
-        (not (or (= 1 arg-count) (= 2 arg-count)))
-        (do (println "Error: Invalid number of arguments. Must be 1 or 2.")
-            (print-usage summary)
-            (js/process.exit 1))
-
-        :else
-        (let [[key-arg value-arg] arguments]
-          (if (= 2 arg-count)
-            ; Write operation
-            (p/do!
-              (write-value sk-bytes key-arg value-arg default-relays)
-              (println (str "Value set for key '" key-arg "'."))
-              (js/process.exit 0))
-            ; Read operation
-            (p/let [retrieved-value (read-value sk-bytes pk-hex key-arg default-relays)]
-              (if retrieved-value
+          ; Read operation
+          (p/let [retrieved-value (read-value sk-bytes pk-hex key-arg default-relays)]
+            (if retrieved-value
+              (do
                 (println retrieved-value)
-                (println (str "No value found for key '" key-arg "'.")))
-              (js/process.exit 0))))))))
+                (js/process.exit 0))
+              (do
+                (js/console.error (str "No value found for key '" key-arg "'."))
+                (js/process.exit 1)))))))))
 
 (defn get-args [argv]
   (not-empty (js->clj (.slice argv
